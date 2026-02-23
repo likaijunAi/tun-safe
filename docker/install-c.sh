cd85e09c8f0a26c95c5ae82cc8fa2eb8c534a53d
leolee <l@xsocket.cn>
2026-02-23 12:57:07 +0800

    1

--- docker/install-c.sh
+++ docker/install-c.sh
@@ -2,11 +2,10 @@ docker run -d \
   --name safe-client \
   --restart always \
   --network host \
-  -v "$(pwd)":/apps \
   -e MODE=client \
   -e UDP_BIND_HOST=0.0.0.0 \
   -e UDP_BIND_PORT=8080 \
   -e REMOTE_TCP_HOST=xx.xx.94.89 \
   -e REMOTE_TCP_PORT=9090 \
   -e DEBUG=true \
-  registry.cn-hangzhou.aliyuncs.com/junhub/openjdk-17-alpine:1.0
+  registry.cn-hangzhou.aliyuncs.com/junhub/tun-safe:1.0.1
